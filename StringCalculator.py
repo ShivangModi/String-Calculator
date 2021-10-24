30f8e9fd41919b3ac10ac490c7317778265563ec
ShivangModi <68642058+ShivangModi@users.noreply.github.com>
2021-10-24 16:08:54 +0530

    now add method can handle an unknown amount of numbers

--- StringCalculator.py
+++ StringCalculator.py
@@ -2,11 +2,8 @@ class StringCalculator:
     def add(self, numbers):
         if len(numbers) == 0:
             return 0
-        elif len(numbers) == 1:
-            return int(numbers)
-        else:
-            num = list(map(int, numbers.split(',')))
-            return sum(num)
+        num = list(map(int, numbers.split(',')))
+        return sum(num)
 
 
 if __name__ == '__main__':
@@ -14,12 +11,13 @@ if __name__ == '__main__':
     # Empty string
     assert(calc.add("") == 0)   # Empty string will return 0
     
-    # only 1 number
+    # for unknown amount of numbers
     assert(calc.add("1") == 1)
     assert(calc.add("10") == 10)
-    
-    # 2 number
     assert(calc.add("1,2") == 3)
     assert(calc.add("2,2") == 4)
+    assert(calc.add("1,2,3") == 6)
+    assert(calc.add("1,2,3,4") == 10)
+    assert(calc.add("1,2,3,4,5") == 15)
     
-    print("ALL TEST CASES PASS FOR 0, 1 OR 2 NUMBERS")
+    print("ALL TEST CASES PASS FOR UNKNOWN AMOUNT OF NUMBERS")
